4e885db743995a1a0ef69170a36ceac338671a4b
Ahmed Ragab Issa <AhmedRagabIssa@gmail.com>
2020-08-16 00:26:47 +0200

    - Remove the unneeded data from the response.

--- foursquare/Entities/FoursquareResponse.swift
+++ foursquare/Entities/FoursquareResponse.swift
@@ -10,64 +10,21 @@ import Foundation
 
 // MARK: - FoursquareResponse
 struct FoursquareResponse: Codable {
-    let meta: Meta?
     let response: Response?
-}
-
-// MARK: - Meta
-struct Meta: Codable {
-    let code: Int?
-    let requestId: String?
 }
 
 // MARK: - Response
 struct Response: Codable {
     let venues: [Venue]?
-    let confident: Bool?
 }
 
 // MARK: - Venue
 struct Venue: Codable {
     let id, name: String?
     let location: Location?
-    let categories: [Category]?
-    let referralId: String?
-    let hasPerk: Bool?
-    let venuePage: VenuePage?
-}
-
-// MARK: - Category
-struct Category: Codable {
-    let id, name, pluralName, shortName: String?
-    let icon: Icon?
-    let primary: Bool?
-}
-
-// MARK: - Icon
-struct Icon: Codable {
-    let prefix, suffix: String?
 }
 
 // MARK: - Location
 struct Location: Codable {
-    let lat, lng: Double?
-    let labeledLatLngs: [LabeledLatLng]?
-    let distance: Int?
-    let cc: String?
-    let city: String?
-    let state: String?
-    let country: String?
-    let formattedAddress: [String]?
-    let address, crossStreet, postalCode: String?
-}
-
-// MARK: - LabeledLatLng
-struct LabeledLatLng: Codable {
-    let label: String?
-    let lat, lng: Double?
-}
-
-// MARK: - VenuePage
-struct VenuePage: Codable {
-    let id: String?
+    let address: String?
 }
